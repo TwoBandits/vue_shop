695f4a25a3c4afbcb95bee0490e2d71d068b207c
Cml <1484254946@qq.com>
2021-09-23 15:11:32 +0800

    feat:添加了页面跳转的进度条展示

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import axios from 'axios';
 import TreeTable from 'vue-table-with-tree-grid';
 // 导入富文本编辑器的插件
 import VueQuillEditor from 'vue-quill-editor';
+import NProgress from 'nprogress';
 
+// elementUI的样式
 import './plugins/element.js';
 import './assets/style/base.css';
 import './assets/style/fonts/iconfont.css';
@@ -13,16 +15,28 @@ import './assets/style/fonts/iconfont.css';
 import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
 import 'quill/dist/quill.bubble.css' // for bubble theme
+// nprogress对应样式
+import 'nprogress/nprogress.css'
 
+import './assets/style/base.css';
+import './assets/style/fonts/iconfont.css';
 
 Vue.config.productionTip = false;
 
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/';
 // axios请求拦截器
 axios.interceptors.request.use(config => {
+  // 在页面展示进度条
+  NProgress.start()
   // 访问授权API时，在请求头中携带token令牌
   config.headers.Authorization = window.sessionStorage.getItem('token');
   return config;
+})
+// axios响应拦截器
+axios.interceptors.response.use(config => {
+  // 在请求响应前关闭进度条
+  NProgress.done()
+  return config
 })
 Vue.prototype.$http = axios;
 
